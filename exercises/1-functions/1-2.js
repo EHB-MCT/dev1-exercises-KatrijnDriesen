6c0030756c1975a1af693c046c37846d3384be6e
Katrijn Driesen <katrijn.driesen@student.ehb.be>
2022-10-08 14:10:13 +0200

    1-2 Created a box with black and red lines

---
+++ exercises/1-functions/1-2.js
@@ -0,0 +1,36 @@
+"use strict";
+
+drawLine();
+
+function drawLine() {
+
+   let myCanvas = document.querySelector("canvas");
+   myCanvas.width = window.innerWidth;
+   myCanvas.height = window.innerHeight;
+   let myContext = myCanvas.getContext("2d");
+
+   //Teken rood kruis
+   myContext.lineWidth = 10;
+   myContext.strokeStyle = 'red';
+   myContext.beginPath();
+   myContext.moveTo(50, 50);
+   myContext.lineTo(250, 250);
+   myContext.stroke();
+  
+   myContext.beginPath();
+   myContext.moveTo(250, 50);
+   myContext.lineTo(50, 250);
+   myContext.stroke();
+ 
+ //Teken zwart vierkant
+   myContext.lineWidth = 10;
+   myContext.strokeStyle = 'black';
+   myContext.beginPath();
+   myContext.moveTo(50, 50);
+   myContext.lineTo(250, 50);
+   myContext.lineTo(250, 250);
+   myContext.lineTo(50, 250);
+   myContext.lineTo(50, 45);
+   myContext.stroke();
+  
+   }
