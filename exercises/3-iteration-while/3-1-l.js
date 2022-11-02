a1dfcb871462f8988244444b5347fdd463878091
Katrijn Driesen <katrijn.driesen@student.ehb.be>
2022-11-02 20:50:02 +0100

    excercise 3-1-l

---
+++ exercises/3-iteration-while/3-1-l.js
@@ -0,0 +1,22 @@
+"use strict";
+import context from "../scripts/context.js";
+import * as Utils from "../scripts/utils.js";
+
+draw();
+
+function draw() {
+    context.lineWidth = 2;
+    context.strokeStyle = "white";
+    context.fillStyle = "#3498DB";
+    context.fillRect(50, 50, 300, 300);
+    drawHourGlass();
+}
+
+function drawHourGlass() {
+    let i = 0;
+    while (i<30) {
+        let x = i*10;
+        Utils.drawLine(50+x,50,350-x,350);
+        i++;
+    }
+}
