58b8a65319092958b49897ac4af2fc14f82b7e26
Katrijn Driesen <katrijn.driesen@student.ehb.be>
2022-11-02 21:52:09 +0100

    excercise 3-1-n

---
+++ exercises/3-iteration-while/3-1-n.js
@@ -0,0 +1,20 @@
+"use strict";
+import context from "../scripts/context.js";
+import * as Utils from "../scripts/utils.js";
+
+draw();
+
+function draw() {
+    context.lineWidth = 2;
+    drawBlockyGradient();
+    context.strokeRect(50, 50, 300, 300);
+}
+
+function drawBlockyGradient() {
+    let i = 0;
+    while (i <= 250) {
+        context.fillStyle = Utils.rgb(i/2.5,i/2.5,i/2.5);
+        context.fillRect(50+i,50,50,300);
+        i = i + 50;
+    }
+}
