5ed24a5bf918ba8adf85135b950f4624034f2175
Katrijn Driesen <katrijn.driesen@student.ehb.be>
2022-11-09 22:04:07 +0100

    excercise 4-1

---
+++ exercises/4-iteration/4-1.js
@@ -0,0 +1,19 @@
+"use strict";
+import context from "../scripts/context.js";
+import * as Utils from "../scripts/utils.js";
+
+context.fillStyle = "orange";
+context.fillRect(0, 0, 600, 300);
+context.strokeStyle = "white";
+
+drawMoreLines ();
+
+function drawMoreLines(){
+for (let i=0;i<60;i++){
+    Utils.drawLine(10*i,0,600-10*i,300);
+    }
+
+for (let j=0;j<=30;j++){
+        Utils.drawLine(0,j*10,600,300-j*10);
+    }
+}
