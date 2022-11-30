f9e5a5b14b594c39fe04bd0d63f3b4191f999a85
Katrijn Driesen <katrijn.driesen@student.ehb.be>
2022-11-30 16:37:21 +0100

    excercise 5-2

---
+++ exercises/5-selection-input-events/5-2a_Maze.js
@@ -0,0 +1,31 @@
+"use strict";
+import context from "../scripts/context.js";
+import * as Utils from "../scripts/utils.js";
+
+let width = context.canvas.width;
+let height = context.canvas.height;
+
+let spacing = 10;
+
+drawMaze();
+
+function drawMaze() {
+    context.lineWidth = 5;
+    context.lineCap = "round";
+
+    let horizontalAmount = width / spacing;
+    let verticalAmount = height / spacing;
+
+    for (let j = 0; j<verticalAmount; j++) {
+
+        for (let i = 0; i<horizontalAmount; i++) {
+            let random = Math.random();
+            
+            if (random > 0.5) {
+                Utils.drawLine(i * spacing, j * spacing, spacing + i * spacing, spacing + j * spacing);
+            } else {
+                Utils.drawLine(spacing + spacing * i, spacing * j, spacing * i, spacing + spacing * j);
+            }
+        }
+    }
+}
