edacc97cf94354b1bd7858c4f50b0670bf503146
Katrijn Driesen <katrijn.driesen@student.ehb.be>
2022-12-02 14:05:59 +0100

    excercise 6-1

--- exercises/6-animation-interaction/6-1_Crosshairs.js
+++ exercises/6-animation-interaction/6-1_Crosshairs.js
@@ -5,3 +5,21 @@ import * as Utils from "../scripts/utils.js";
 let width = context.canvas.width;
 let height = context.canvas.height;
 
+window.onmousemove = crosshairs;
+
+context.strokeStyle = "red";
+context.lineWidth = 5;
+
+/**
+*
+* @param {MouseEvent} eventData
+*/
+
+function crosshairs (eventData){
+    let x = eventData.pageX;
+    let y = eventData.pageY;
+    context.fillStyle = "white";
+    context.fillRect(0,0,width, height);
+    Utils.drawLine(x,0,x,height);
+    Utils.drawLine(0,y,width,y);
+}
