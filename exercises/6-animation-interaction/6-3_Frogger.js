3c79377d95622c3371057e3f22e04773f0eaea25
Katrijn Driesen <katrijn.driesen@student.ehb.be>
2022-12-05 14:46:05 +0100

    6-3 moving frog & cars

--- exercises/6-animation-interaction/6-3_Frogger.js
+++ exercises/6-animation-interaction/6-3_Frogger.js
@@ -5,12 +5,70 @@ import * as Utils from "../scripts/utils.js";
 let width = context.canvas.width;
 let height = context.canvas.height;
 let heightStreet = height / 6;
+let moveFrogY = 0;
+let moveFrogX = 0;
+let posFrogX = width / 2;
+let posFrogY = height * 5.5 / 6;
+let distancePaw = 17;
+let isPlaying = 1;
+let posXCarA = 0;
+let posXCarB = width - 150;
+let spaceWindow = 5;
+let heigthCar = height / 10;
+let speed = 1;
+
+window.onkeydown = moveFrog;
+
+/**
+*
+* @param {KeyboardEvent} eventData
+*/
 
 setup ();
-//update ();
+update ();
 
 function setup (){
-    //draw background
+    
+    drawBackground ();   
+
+    drawFrog ();
+
+    drawCars ();
+}
+
+function moveFrog (eventData) {
+    if (eventData.code == "ArrowUp") {
+        moveFrogY -= 10;
+    } else if (eventData.code == "ArrowDown") {
+        moveFrogY += 10;
+    } else if (eventData.code == "ArrowLeft") {
+        moveFrogX -= 10;
+    } else if (eventData.code == "ArrowRight") {
+        moveFrogX += 10;
+    }
+}
+
+function update() {
+    if (isPlaying == 1) {
+        
+        drawBackground ();    
+        
+        drawFrog ();
+    
+        drawCars ();
+                       
+        //move cars
+        posXCarA += speed;
+        posXCarB -= speed;
+        if (posXCarA >= width-150 || posXCarA <= 0) {
+            speed *= -1;
+        }
+
+        requestAnimationFrame (update);
+    }
+}
+
+function drawBackground () {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, width, height);
     context.fillStyle = "grey";
@@ -18,24 +76,19 @@ function setup (){
     context.fillRect(0, height * 3 / 4, width, -heightStreet);
     context.fillStyle = "lightgrey";
     context.fillRect(0, height / 4 + height / 6, width, heightStreet);
+}
 
-    //draw frog
-    let frogX = width / 2;
-    let frogY = height * 5.5 / 6;
-    let distancePaw = 17;
+function drawFrog () {
     context.fillStyle = "green";
-    Utils.fillCircle (frogX, frogY, 20);
-    Utils.fillCircle (frogX, frogY - 27, 10);
-    Utils.fillCircle (frogX + distancePaw, frogY + distancePaw, 5);
-    Utils.fillCircle (frogX + distancePaw, frogY - distancePaw, 5);
-    Utils.fillCircle (frogX - distancePaw, frogY - distancePaw, 5);
-    Utils.fillCircle (frogX - distancePaw, frogY + distancePaw, 5);
+    Utils.fillCircle (posFrogX + moveFrogX, posFrogY + moveFrogY, 20);
+    Utils.fillCircle (posFrogX + moveFrogX, posFrogY - 27 + moveFrogY, 10);
+    Utils.fillCircle (posFrogX + distancePaw + moveFrogX, posFrogY + distancePaw + moveFrogY, 5);
+    Utils.fillCircle (posFrogX + distancePaw + moveFrogX, posFrogY - distancePaw + moveFrogY, 5);
+    Utils.fillCircle (posFrogX - distancePaw + moveFrogX, posFrogY - distancePaw + moveFrogY, 5);
+    Utils.fillCircle (posFrogX - distancePaw + moveFrogX, posFrogY + distancePaw + moveFrogY, 5);
+}
 
-    //draw cars
-    let posXCarA = 0;
-    let posXCarB = width - 150;
-    let spaceWindow = 5;
-    let heigthCar = height / 10;
+function drawCars () {
     context.fillStyle = "red";
     context.fillRect(posXCarA, height * 17 / 60, 150, heigthCar);
     context.fillRect(posXCarB, height * 37 / 60, 150, heigthCar);
@@ -45,8 +98,4 @@ function setup (){
     context.fillRect(posXCarB + spaceWindow, height * 37 / 60 + spaceWindow, 50, heigthCar - spaceWindow * 2);
     context.fillRect(posXCarB + 120, height * 37 / 60 + spaceWindow, 30-spaceWindow, heigthCar - spaceWindow * 2);
 }
-
-//if {speed cars -1}
-//requestAnimationFrame
-//onmouseup, down, left, right --> x,y pos frog
-//redraw background
+//hitcar
