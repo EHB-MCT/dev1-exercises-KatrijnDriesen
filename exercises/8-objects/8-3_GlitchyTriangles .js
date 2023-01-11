fe862b514dc096ddbf6726cc94b24ac1439514a4
Katrijn Driesen <katrijn.driesen@student.ehb.be>
2023-01-11 17:39:21 +0100

    8-3

--- exercises/8-objects/8-3_GlitchyTriangles .js
+++ exercises/8-objects/8-3_GlitchyTriangles .js
@@ -12,58 +12,57 @@ setup();
 draw();
 
 function setup () {
-    for (let i = 0; i < 100; i++) {
-        let scale = 100;
-        let x1: Utils.randomNumber(0, width);
-        let y1: Utils.randomNumber(0, height);
-        let x2: x1 + Utils.randomNumber(0, width);
-        let y2: y1 + Utils.randomNumber(0, height);
-        let x3: x1 + Utils.randomNumber(0, width);
-        let y3: y1 + Utils.randomNumber(0, height);
+    for (let i = 0; i < 200; i++) {
+        let scale = 40;
+        let x1 = Utils.randomNumber(0, width);
+        let y1 = Utils.randomNumber(0, height);
+        let x2 = x1 + Utils.randomNumber(-scale, scale);
+        let y2 = y1 + Utils.randomNumber(-scale, scale);
+        let x3 = x2 + Utils.randomNumber(-scale, scale);
+        let y3 = y2 + Utils.randomNumber(-scale, scale);
 
-            let triangle = {
-                x1: x1,
-                x2: x2,
-                x3: x3,
-                y1: y1,
-                y2: y2,
-                y3: y3,
-                hue: Utils.randomNumber(0, 360)
-            }
-        }
-
-        triangles.push(square);
+        let triangle = {
+            x1 : x1,
+            y1 : y1,
+            x2 : x2,
+            y2 : y2,
+            x3 : x3,
+            y3 : y3,
+            r : Utils.randomNumber(0, 10),
+            g : Utils.randomNumber(20, 100),
+            b : Utils.randomNumber(20, 100)
+        };
+        triangles.push(triangle);
     }
+}
 
 function draw() {
-        context.fillStyle = "white";
-        context.fillRect(0, 0, width, height);
-
-        for (let i = 0; i < triangles.length; i++) {
-            let triangle = triangles [i];
-            
-            //draw
-            context.fillStyle = Utils.hsla(square.hue, 50, 50, 50);
-            context.beginPath();
-            context.moveTo();
-            context.lineTo
-            context.lineTo
-            context.fill();
-
-            //update
-            updateTriangle(triangle);
-
-        }
-
-        requestAnimationFrame (draw);
+    context.fillStyle = Utils.rgb(20,50,50);
+    context.fillRect(0, 0, width, height);
+    
+    for (let i = 0; i < triangles.length; i++) {
+    let triangle = triangles[i];
+    
+    //draw
+    context.fillStyle = Utils.rgba(triangle.r,triangle.g,triangle.b,50);
+    context.beginPath();
+    context.moveTo(triangle.x1, triangle.y1);
+    context.lineTo(triangle.x2, triangle.y2);
+    context.lineTo(triangle.x3, triangle.y3);
+    context.fill();
+    context.fillRect(triangle.x, triangle.y, triangle.size, triangle.size);
+    
+    //update
+    updateTriangle(triangle);
+}
+    requestAnimationFrame(draw);
 }
 
 function updateTriangle(triangle) {
-    triangle.x1 += Utils.randomNumber(-2, 2);
-    triangle.y1 += Utils.randomNumber(-2, 2);
-    triangle.x2 += Utils.randomNumber(-2, 2);
-    triangle.y2 += Utils.randomNumber(-2, 2);
-    triangle.x3 += Utils.randomNumber(-2, 2);
-    triangle.y3 += Utils.randomNumber(-2, 2);
-
+    triangle.x1 += Utils.randomNumber(-1, 1);
+    triangle.y1 += Utils.randomNumber(-1, 1);
+    triangle.x2 += Utils.randomNumber(-1, 1);
+    triangle.y2 += Utils.randomNumber(-1, 1);
+    triangle.x3 += Utils.randomNumber(-1, 1);
+    triangle.y3 += Utils.randomNumber(-1, 1);
 }
