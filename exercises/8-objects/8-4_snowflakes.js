d85b861f21ac4dd277c9edeaec44f5ec83d561d4
Katrijn Driesen <katrijn.driesen@student.ehb.be>
2023-01-12 11:33:50 +0100

    8-4

--- exercises/8-objects/8-4_snowflakes.js
+++ exercises/8-objects/8-4_snowflakes.js
@@ -5,33 +5,39 @@ import * as Utils from "../scripts/utils.js";
 let width = context.canvas.width;
 let height = context.canvas.height;
 
-//draw loop + user input
+window.onmousemove = updateWind;
 
 let snowFlakes = [];
-let wind = 10;
+let wind = 0.5;
 
 setup();
 draw();
 
 function setup() {
-    for (let i = 0; i < 200; i++) {
-        let snowFlake = {
-            x: Utils.randomNumber(0, width),
-            y: 0,
-            size: Utils.randomNumber(),
-            alpha: Utils.randomNumber(20,60),
-            speed: 1 + Math.random() * 5
-        };
-
-    snowFlakes.push(snowFlake);
+    for (let i = 0; i < 100; i++) {
+        createSnowFlakes();
     }
 }
 
-function draw() {
-    context.fillStyle = "red";
-    context.fillRect(0, 0, width, height);
+function createSnowFlakes(){
+    let snowFlake = {
+        x: Utils.randomNumber(-200, width),
+        y: 0,
+        size: Utils.randomNumber(10, 50),
+        alpha: Utils.randomNumber(20, 60),
+        speed: Math.random()
+    };
 
-    context.strokeStyle = "white";
+    snowFlakes.push(snowFlake);
+
+/*     if (snowFlake.y > height){
+        snowFlakes.shift();
+    } */
+}
+
+function draw() {
+    context.fillStyle = "DarkSlateGray";
+    context.fillRect(0, 0, width, height);
 
     for (let i = 0; i < snowFlakes.length; i++) {
         let snowFlake = snowFlakes[i];
@@ -42,17 +48,33 @@ function draw() {
         snowFlake.y += snowFlake.speed;
         snowFlake.x += wind;
     }
+
+    //Add extra snowflakes
+    for (let i = 0; i < 1; i++) {
+        createSnowFlakes();
+    }
+    //console.log(snowFlakes.length);
     requestAnimationFrame(draw);
 }
 
 function drawSnowFlake(x, y, size) {
     let half = size / 2;
-    let thirt = size / 3;
-    context.lineWidth = size / 5;
+    let third = size / 3;
+    context.lineWidth = size / 7;
     context.beginPath();
-    context.moveTo(x,y-half);
-    context.lineTo(x,y+half);
-    context.moveTo(x-half,y);
-    context.lineto(x);
+    context.moveTo(x, y - half);
+    context.lineTo(x, y + half);
+    context.moveTo(x - half, y);
+    context.lineTo(x + half, y);
+    context.moveTo(x - third, y - third);
+    context.lineTo(x + third, y + third);
+    context.moveTo(x - third, y + third);
+    context.lineTo(x + third, y - third);
     context.stroke();
+}
+
+function updateWind(e) {
+    console.log(e.pageX);
+    let scaler = 400;
+    wind = (width / 2 - e.pageX) / scaler;
 }
